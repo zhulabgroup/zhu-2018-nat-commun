c72c9ad38e6f610ba6708c233b9632cc3cbcfb13
Kai Zhu <kai.zhu@socs-stats.ucsc.edu>
2020-11-18 21:40:00 -0800

    add install packages

--- code/00-setup.R
+++ code/00-setup.R
@@ -1,3 +1,12 @@
+# install packages
+list.of.packages <- c(
+  "tidyverse", "scales", "plotrix", "modelr",
+  "R2jags", "ggmcmc", # also install jags http://mcmc-jags.sourceforge.net
+  "tmap", "sp", "SpatialPack", "raster"
+)
+new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[, "Package"])]
+if (length(new.packages)) install.packages(new.packages)
+
 # set paths for input & output
 file.symlink("/data/ZHULAB/FIA/Growth/Data/", "data")
 file.symlink("/data/ZHULAB/FIA/Growth/Models/", "models")
